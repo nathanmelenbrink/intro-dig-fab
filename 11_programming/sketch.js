674fcb54c09e0700acfe3abec233b9be4e711f03
Nathan Melenbrink <nathanmelenbrink@gmail.com>
2020-07-28 15:19:46 -0400

    sketch

--- 11_programming/sketch.js
+++ 11_programming/sketch.js
@@ -101,11 +101,11 @@ function setup() {
 
 // Got the list of ports
 function gotList(thelist) {
-  println("List of Serial Ports:");
+  //println("List of Serial Ports:");
   // theList is an array of their names
   for (var i = 0; i < thelist.length; i++) {
     // Display in the console
-    println(i + " " + thelist[i]);
+    //println(i + " " + thelist[i]);
   }
 }
 
